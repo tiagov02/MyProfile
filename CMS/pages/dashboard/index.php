fbaaa823a385c3fad35d1da6c4780a27f2c2b81c
Tiago Viana <tiagoviana.1003@gmail.com>
2022-12-22 19:18:50 +0000

    update

---
+++ CMS/pages/dashboard/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require "../../utils/templates.php";
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+    <h2>Dashboard</h2>
+    <p>Hello to my CMS!</p>
+</div>
+
+<?=template_footer()?>
