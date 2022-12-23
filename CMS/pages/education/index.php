3a86fc55bd485c6a96d5f74f66a58a191c4c56b5
Tiago Viana <tiagoviana.1003@gmail.com>
2022-12-23 23:48:17 +0000

    add index(read to the CMS)

---
+++ CMS/pages/education/index.php
@@ -0,0 +1,49 @@
+<?php
+
+require "../../utils/templates.php";
+
+?>
+<?php
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true ):
+    template_header('Home')
+    ?>
+
+    <div>
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <!--REPLACE TO THE FOR CICLE-->
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <?=template_footer()?>
+<?php else:
+    header(" location: ../auth");
+endif;
+?>
