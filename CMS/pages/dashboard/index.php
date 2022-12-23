c4a6b0bb98f287a776bbd46ecd6fd64b0b93c409
Tiago Viana <tiagoviana.1003@gmail.com>
2022-12-23 00:44:49 +0000

    update

--- CMS/pages/dashboard/index.php
+++ CMS/pages/dashboard/index.php
@@ -3,7 +3,7 @@
 require "../../utils/templates.php";
 
 ?>
-
+<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true ): ?>
 <?=template_header('Home')?>
 
 <div class="content">
@@ -12,3 +12,7 @@ require "../../utils/templates.php";
 </div>
 
 <?=template_footer()?>
+<?php else:
+    header(" location: ../auth");
+endif;
+?>
