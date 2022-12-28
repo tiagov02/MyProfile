5ec1c718314ef8d264d9bdb272bdc31726985c37
Tiago Viana <tiagoviana.1003@gmail.com>
2022-12-28 15:31:11 +0000

    update --> upd WORKING

--- CMS/pages/education/update.php
+++ CMS/pages/education/update.php
@@ -5,37 +5,32 @@ require "../../DB/connectDB.php";
 
 $pdo = pdo_connect_mysql();
 //VER SESSION
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+
     if(isset($_GET['id'])) {
-        if(!empty($POST)) {
-            // Post data not empty insert a new record
-            // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
-            $place = isset($_POST['place']) ? $_POST['place'] : '';
-            $description = isset($_POST['description']) ? $_POST['description'] : '';
-            $year_ini = isset($_POST['year_ini']) ? $_POST['year_ini'] : '';
-            $year_end = isset($_POST['year_end']) ? $_POST['year_end'] : '';
-            // Insert new record into the languages table
-            /**
-             * INSERT INTO bd_sir.education (id, place, description, year_ini, year_end)
-             */
-            //$stmt = $pdo->prepare('INSERT INTO education(place, description, year_ini, year_end) VALUES (?, ?, ?, ?)');
-            $stmt = $pdo->prepare('UPDATE education set place=?,description=?,year_ini=?,year_end=? WHERE id=?');
-            $stmt->execute([$place, $description, $year_ini, $year_end, $_GET['id']]);
-            // Output message
-            $msg = 'UPDATED Successfully!';
-        }
-        // Get the language from the languages table
         $stmt = $pdo->prepare('SELECT * FROM education WHERE id = ?');
         $stmt->execute([$_GET['id']]);
         $education = $stmt->fetch(PDO::FETCH_ASSOC);
-        if (empty($education)) {
-            exit('That record with that ID doesn\'t exist!');
+        if (!$education) {
+            exit('Language doesn\'t exist with that ID!');
         }
-    }
-    else{
-        exit("Unauthorized please come back<a href=\"index.php\">here</a>!");
-    }
-}
+        if (!empty($POST)) {
+               // Post data not empty insert a new record
+               // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
+               $place = isset($_POST['place']) ? $_POST['place'] : '';
+               $description = isset($_POST['description']) ? $_POST['description'] : '';
+               $year_ini = isset($_POST['year_ini']) ? $_POST['year_ini'] : '';
+               $year_end = isset($_POST['year_end']) ? $_POST['year_end'] : '';
+               // Insert new record into the languages table
+               /**
+                * INSERT INTO bd_sir.education (id, place, description, year_ini, year_end)
+                */
+               //$stmt = $pdo->prepare('INSERT INTO education(place, description, year_ini, year_end) VALUES (?, ?, ?, ?)');
+               $stmt = $pdo->prepare('UPDATE education set place=?,description=?,year_ini=?,year_end=? WHERE id=?');
+               $stmt->execute([$place, $description, $year_ini, $year_end, $_GET['id']]);
+               // Output message
+                $msg = 'UPDATED Successfully!';
+            }
+        }
 ?>
 
 <?=template_header('Education');?>
@@ -43,7 +38,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <h2>Create language</h2>
     <form action="create.php" method="post">
         <label for="place">Place</label>
-        <input type="text" name="place" placeholder="Viana do Castelo" id="name" class="form-control" value="<?=$education['name']?>">
+        <input type="text" name="place" placeholder="Viana do Castelo" id="name" class="form-control" value="<?=$education['place']?>">
         <label for="year_ini">Year of start</label>
         <input type="number" name="year_ini" placeholder="2022" id="year_ini" class="form-control" value="<?=$education['year_ini']?>">
         <label for="year_end">Year of end</label>
@@ -57,5 +52,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <?php endif; ?>
 </div>
 
-<?=template_footer();?>
+<?=template_footer();?>-->
 
