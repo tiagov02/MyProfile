9ea01a7af1bd361b96e834c77affa14a14bb4e2a
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-07 18:32:25 +0000

    update

--- libraries/mail.php
+++ libraries/mail.php
@@ -2,7 +2,12 @@
 $to_email = "jtiagoviana@ipvc.pt";
 $subject = "Test email to send from XAMPP";
 $body = "Hi, This is test mail to check how to send mail from Localhost Using Gmail ";
-$headers = "sirtp_dev@sapo.pt";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
 
 if (mail($to_email, $subject, $body, $headers))
 
