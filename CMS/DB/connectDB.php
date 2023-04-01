038d8f2e2deed0ef3678b1359bae30699fc12713
Tiago Viana <tiagoviana.1003@gmail.com>
2023-04-01 18:18:14 +0100

    - update

--- CMS/DB/connectDB.php
+++ CMS/DB/connectDB.php
@@ -1,10 +1,10 @@
 <?php
 pdo_connect_mysql();
 function pdo_connect_mysql() {
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'bd_sir';
+    $DATABASE_HOST = '127.0.0.1';
+    $DATABASE_USER = 'sir';
+    $DATABASE_PASS = 'sir';
+    $DATABASE_NAME = 'sir';
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname='
             . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
